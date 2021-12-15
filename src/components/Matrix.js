69e8f1443c137b576e82950963664db7be7b6975
Miika Halmetoja <mhalmetoja@gmail.com>
2021-12-15 05:42:09 +0200

    Working on vertical CSS class -injections

--- src/components/Matrix.js
+++ src/components/Matrix.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from 'react';
+import React, { Component } from 'react';
 
 class Matrix extends Component {
 
@@ -6,13 +6,21 @@ class Matrix extends Component {
       super(props)
       this.inputStringToCharArrayParser = this.inputStringToCharArrayParser.bind(this)
       this.characterArrayToBooleanParser = this.characterArrayToBooleanParser.bind(this)
+      this.inputStringToNumberArrayParser = this.inputStringToNumberArrayParser.bind(this)
+      this.mappingVerticalsToMappedArray = this.mappingVerticalsToMappedArray.bind(this)
       this.state = {
-        stringInput: "hitomezashi",
+        stringInput: "uoyhtiwebecrofehtyam",
+        numberInput: "314159265359",
       }
   }
 
   componentDidMount () {
 
+  }
+
+  inputStringToNumberArrayParser (numberInput) {
+    let numberArray = numberInput.split('')
+    return numberArray
   }
 
   inputStringToCharArrayParser (stringInput) {
@@ -47,19 +55,25 @@ class Matrix extends Component {
   }
 
   mappingTwoDimensionalArray (twoDimensionalArray, booleanArray) {
-    let iterator = 0;
+    let rowIterator = 0;
     let table = twoDimensionalArray.map((array) => { 
       //"For each array in the twoDimensionalArray -array, iterate through each of the <td>{cell}</td> elements in the table object and conditionally return a replacement with correct className value."
       let elementArrayToReturn = [];
+      let numberInputArrayToIterate = this.inputStringToNumberArrayParser(this.state.numberInput)
+      let cellIterator = 0;
       array.forEach(element => {
         //Check if the innerHTML value is "even" or "odd" and return a table cell with the className accordingly
-        if(booleanArray[iterator] === true) {
-          elementArrayToReturn.push(<td className="even">{element}</td>)
+        if(booleanArray[rowIterator] === true && cellIterator % 2 === 1) {
+          elementArrayToReturn.push(<td className="borderBottom">{`${booleanArray[rowIterator]}, ${numberInputArrayToIterate[cellIterator]}`}</td>)
+        } else if(booleanArray[rowIterator] === false && cellIterator % 2 === 0) {
+          elementArrayToReturn.push(<td className="borderBottom">{`${booleanArray[rowIterator]}, ${numberInputArrayToIterate[cellIterator]}`}</td>)
         } else {
-          elementArrayToReturn.push(<td className="odd">{element}</td>)
+          elementArrayToReturn.push(<td className="teal">{`${booleanArray[rowIterator]}, ${numberInputArrayToIterate[cellIterator]}`}</td>)
         }
+        console.log(cellIterator)
+        cellIterator++
       })
-      iterator++
+      rowIterator++
       //Return a JSX array back to the 2-dimensional "table" -array, as a replacement of the original arrays with simple cell values.
       return (
         <tr className = "tableRow">
@@ -68,7 +82,23 @@ class Matrix extends Component {
       )
   
     })
-    return table
+    return this.mappingVerticalsToMappedArray(table)
+  }
+
+  mappingVerticalsToMappedArray (table) {
+    let rdyTable = table
+    console.log(table)
+    //Iterate through the cells of the first array (or use the numberInput array) and check the modulo of every cell in row. If it returns false,
+    //let's say [0,0] equals false, inject [1,0........word.length,0] <td>'s variably with rightBorder. Set a boolean to true, conditionally
+    //inject the <td> if boolean is true, and return it as is, if false. Do this until length of the numberInput and return table with
+    //correct classnames.
+
+    //create a function that works as the "boolean machine", for example checker(array) that injects the classes accordingly depending
+    //whether the first value is odd or even. Effectively we open the table arrays and re-map them accordingly to a new table.
+    //See if you can look into the classNames of the <td>'s. If there's already a bottomBorder class and you should be injecting
+    //the rightBorder class, return a version which has both, else just return the rightBorder.
+
+    return rdyTable
   }
 
   onSubmit(e) {
@@ -81,7 +111,7 @@ class Matrix extends Component {
   render () {
     let charArray = this.inputStringToCharArrayParser(this.state.stringInput)
     let booleanArray = this.characterArrayToBooleanParser(charArray)
-    let twoDimensionalArray = this.twoDimensionalArrayGenerator(this.state.stringInput.length, this.state.stringInput.length) // Row, Col
+    let twoDimensionalArray = this.twoDimensionalArrayGenerator(this.state.stringInput.length, this.state.numberInput.length) // Row, Col
     let table = this.mappingTwoDimensionalArray(twoDimensionalArray, booleanArray)
     return (
       <div>
