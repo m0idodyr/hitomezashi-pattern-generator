af1d04b8a1793924ec4819a0f87f39f562f4ba54
Miika Halmetoja <mhalmetoja@gmail.com>
2021-12-20 19:25:48 +0200

    Element keys, gate for inputs, conditional rendering

--- src/components/Hitomezashi.js
+++ src/components/Hitomezashi.js
@@ -8,6 +8,7 @@ class Hitomezashi extends Component {
     this.characterArrayToBooleanParser = this.characterArrayToBooleanParser.bind(this);
     this.inputStringToNumberArrayParser = this.inputStringToNumberArrayParser.bind(this);
     this.mappingVerticalsToMappedArray = this.mappingVerticalsToMappedArray.bind(this);
+
     this.state = {
       stringInput: "hitomezashipatterngenerator",
       numberInput: "31415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679",
@@ -64,22 +65,22 @@ class Hitomezashi extends Component {
    */
   mappingTwoDimensionalArray (twoDimensionalArray, booleanArray) {
     let rowIterator = 0;
-    let table = twoDimensionalArray.map((array) => { 
+    let table = twoDimensionalArray.map((array, key) => { 
       let elementArrayToReturn = [];
       let cellIterator = 0;
-      array.forEach(() => {
+      for (let i = 0; i < array.length; i++) {
         if(booleanArray[rowIterator] === true && cellIterator % 2 === 1) {
-          elementArrayToReturn.push(<td className="borderBottom"></td>);
+          elementArrayToReturn.push(<td key={i} className="borderBottom"></td>);
         } else if(booleanArray[rowIterator] === false && cellIterator % 2 === 0) {
-          elementArrayToReturn.push(<td className="borderBottom"></td>);
+          elementArrayToReturn.push(<td key={i} className="borderBottom"></td>);
         } else {
-          elementArrayToReturn.push(<td className="noBorder"></td>);
+          elementArrayToReturn.push(<td key={i} className="noBorder"></td>);
         }
         cellIterator++;
-      })
+      }
       rowIterator++;
       return (
-        <tr className = "tableRow">
+        <tr key={key} className = "tableRow">
           {elementArrayToReturn}
         </tr>
       )
@@ -95,20 +96,20 @@ class Hitomezashi extends Component {
    */
   mappingVerticalsToMappedArray (table) {
     let boolean = true;
-    let finalArray = table.map((array => {
+    let finalArray = table.map((array, key) => {
       let elementArrayToReturn = [];
       for (let i = 0; i < array.props.children.length; i++) {
         if (this.state.numberInput[i] % 2 === 0 && boolean === true) {
           if(array.props.children[i].props.className === "borderBottom") {
-            elementArrayToReturn.push(<td className="borderBottomBorderRight"></td>);
+            elementArrayToReturn.push(<td key={i} className="borderBottomBorderRight"></td>);
           } else {
-            elementArrayToReturn.push(<td className="borderRight"></td>);
+            elementArrayToReturn.push(<td key={i} className="borderRight"></td>);
           }
         } else if (this.state.numberInput[i] % 2 === 1 && boolean === false) {
           if(array.props.children[i].props.className === "borderBottom") {
-            elementArrayToReturn.push(<td className="borderBottomBorderRight"></td>);
+            elementArrayToReturn.push(<td key={i} className="borderBottomBorderRight"></td>);
           } else {
-            elementArrayToReturn.push(<td className="borderRight"></td>);
+            elementArrayToReturn.push(<td key={i} className="borderRight"></td>);
           }
         } else {
           elementArrayToReturn.push(array.props.children[i]);
@@ -116,11 +117,11 @@ class Hitomezashi extends Component {
       }
       boolean = !boolean;
       return (
-        <tr className = "tableRow">
+        <tr key={key} className = "tableRow">
           {elementArrayToReturn}
         </tr>
       )
-    }))
+    })
     return finalArray;
   }
 
@@ -139,19 +140,35 @@ class Hitomezashi extends Component {
   }
 
   render () {
-    let charArray = this.inputStringToCharArrayParser(this.state.stringInput);
-    let booleanArray = this.characterArrayToBooleanParser(charArray);
-    let twoDimensionalArray = this.twoDimensionalArrayGenerator(this.state.stringInput.length, this.state.numberInput.length);
-    let table = this.mappingTwoDimensionalArray(twoDimensionalArray, booleanArray);
-    return (
-      <div>
-        <div className="inputWrapper">
-          <input id="stringInputField" value={this.state.stringInput} onChange={e => this.changeStringInput(e)} />
-          <input id="numberInputField" value={this.state.numberInput} onChange={e => this.changeNumberInput(e)} />
+    if (this.state.stringInput.length >= 2 && this.state.numberInput.length >= 2) {
+      let charArray = this.inputStringToCharArrayParser(this.state.stringInput);
+      let booleanArray = this.characterArrayToBooleanParser(charArray);
+      let twoDimensionalArray = this.twoDimensionalArrayGenerator(this.state.stringInput.length, this.state.numberInput.length);
+      let table = this.mappingTwoDimensionalArray(twoDimensionalArray, booleanArray);
+      return (
+        <div>
+          <div className="inputWrapper">
+            <input id="stringInputField" value={this.state.stringInput} onChange={e => this.changeStringInput(e)} />
+            <input id="numberInputField" value={this.state.numberInput} onChange={e => this.changeNumberInput(e)} />
+          </div>
+          <table className = "table">
+            <tbody>
+              {table}
+            </tbody>
+          </table>
         </div>
-        <table className = "table">{table}</table>
+      )
+    } else {
+      return (
+        <div>
+          <div className="inputWrapper">
+            <input id="stringInputField" value={this.state.stringInput} onChange={e => this.changeStringInput(e)} />
+            <input id="numberInputField" value={this.state.numberInput} onChange={e => this.changeNumberInput(e)} />
+          </div>
+        <div className="typeInAtLeastMessage">Type in at least two letters and digits in order to begin</div>
       </div>
-    )
+      )
+    }
   }
 }
 
